662e2626125f3479b66946b02c963b4723730147
shihao wang <913003120@qq.com>
2021-11-06 17:57:42 +0800

    feat: 添加demo

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import * as styles from './App.css';
+
+const App: FC = () => {
+  return <div className={styles.container}>test app</div>;
+};
+
+export default App;
